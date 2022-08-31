91d98dbdd7a3be6aaab47ad4f880f1b9c4b24bff
Aluno de Front End Senai <AlunoDeFrontEndSenai@gmail.com>
2022-08-31 16:32:19 -0300

    primeira parte finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { ProdutoComponent } from './views/produto/produto.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    ProdutoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,16 @@
+
+// jQuery
+$(document).ready(function () {
+    // Todas as funções do jQuery
+
+    $(".btn-buy").click(function(){
+        alert("Este produto não está disponível");
+    });
+
+       
+});
+
+
 function abreFechaMenu() {
     // navbar -tag nav
     let navbar = document.querySelector(".menu");
