1e0a5bcd9a39929bcfd810f5fffcea87a3ab8201
Aluno de Front End Senai <AlunoDeFrontEndSenai@gmail.com>
2022-09-06 18:07:41 -0300

    Retorno erros tratamento excecoes

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -15,23 +15,89 @@ export class LoginComponent implements OnInit {
   faArrowLeft = faArrowLeft
 
   userModel = new User()
+  nomeProfessor:any = ""
+  mensagem: string = ""
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
+  mostrarNome(){
+    this.nomeProfessor = this.userModel.nome;
+  }
+
+  validaLogin():boolean{
+    if(this.userModel.nome === undefined || this.userModel.nome === '' ||
+      this.userModel.email === undefined || this.userModel.email === '' ||
+      this.userModel.password === undefined || this.userModel.password === '' 
+      ){
+
+      // console.log(this.userModel);
+      return false;
+    } else{
+      return true;
+  }
+}
 
   //Função do login
 
   signin(){
-    // console.log(this.userModel);
+    
+    //Fazer validação
+    if(this.validaLogin()){
 
-    this.userService.sigin(this.userModel).subscribe(function(response){
-      console.log(response);
+      console.log(this.userModel);
 
-    })
-  }
+      this.userService.sigin(this.userModel)
+      .subscribe(
+        {
+          next: (response)=>{
+            console.log(response);
+            this.mensagem = `Logado com Sucesso! ${response.status} ${response.statusText}`
+      
+          },
+          error: (e)=>{
+            console.log('Deu ruim',e);
+            // console.clear()
+            this.mensagem = `${e.error} ${e.status} ${e.statusText}`
+      
+          } 
+        }
+      )
 
-}
+      console.log(this.userModel);
+      this.mensagem = "Preencher todos os campos"
+    
 
+    } else{//Falta preencher campos
+      console.log(this.userModel);
+      this.mensagem = "Preencher todos os campos"
+
+    }
+     
+    } 
+
+  } //Fim da Classe
+
+
+
+// Nova forma de utilizar o subscribe segundo a bilbioteca rxJS
+//https://rxjs.dev/deprecations/subscribe-arguments
+//   subscribe({
+//     next: (v) => console.log(v),
+//     error: (e) => console.error(e),
+//     complete: () => console.info('complete') 
+// })
+      //Arrow Functions
+      // (response)=>{
+      // console.log(response);
+      // this.mensagem = `Logado com Sucesso! ${response.status} ${response.statusText}`
+
+    // })
+ 
+  // }
+
+// (params)=> {
+
+//} 
